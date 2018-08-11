5ce43b5f629f045bd1083ee7634021780cc0be92
Aman Kumar Yadav <yadav.aman099@gmail.com>
2018-08-11 21:54:37 +0530

    TownStruct added
    #3rd person character with move animation and jump mechanics. Includes terrain and the modelling is done by magicaVoxel.

---
+++ Townstruct/Assets/3rd Person mover/Anim/Controls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+    static Animator anim;
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetButtonDown("Jump"))
+
+        { anim.SetTrigger("isJumping"); }
+
+        
+
+
+        if (translation != 0)
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isIdle", false);
+        }
+        else
+        {  anim.SetBool("isRunning", false);
+        anim.SetBool("isIdle", true);
+    }
+
+}
+}
