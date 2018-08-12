dfbfe1ce577d33c394c1ab0e80a4c58cefd34534
Aman Kumar Yadav <yadav.aman099@gmail.com>
2018-08-12 12:38:35 +0530

    Adding a menu to the game
    adding a menu with start | options | quit

---
+++ Pong/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour {
+
+	public void Play(){
+		
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+			Debug.Log ("space pressed");
+
+
+	}
+	public void Quit(){
+		Application.Quit ();
+	}
+}
